33a7828dff6d0435eed85faff5b82406105a65d5
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-04-30 15:41:52 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/todos")
+      .then((response) => {
+        setTodos(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching todos:", error);
+      });
+  }, []);
+
+  return <>{JSON.stringify(todos)}</>;
+}
+
+export default App;
